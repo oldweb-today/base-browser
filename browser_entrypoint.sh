3f98dbbea62d6cc4a20b826bc31503b74fc9db90
Ilya Kreymer <ikreymer@users.noreply.github.com>
2019-04-09 14:20:43 -0700

    broser entrypoint: improvements for loading CA cert: (#4)
    * broser entrypoint: improvements for loading CA cert:
    - if $PROXY_CA_FILE is set and exists as a file, use that as the CA cert
    - if $PROXY_CA_FILE and $PROXY_CA_URL are set, curl $PROXY_CA_URL and store at $PROXY_CA_FILE,
    waiting for $PROXY_HOST:$PROXY_PORT to become available
    
    * tweaks from shellcheck lint

--- browser_entrypoint.sh
+++ browser_entrypoint.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-if [[ -n "$PROXY_HOST" ]]; then
+if [[ -n "$PROXY_HOST" && -n "$PROXY_PORT" ]]; then
     # resolve to ip now, if possible
-    IP=$(host $PROXY_HOST | head -n 1 | cut -d ' ' -f 4)
-    if (( $? == 0 )); then
+    if IP=$(host "$PROXY_HOST" | head -n 1 | cut -d ' ' -f 4); then
         export PROXY_HOST=$IP
         echo "IP: $IP"
+    fi
+
+    # if PROXY_CA_FILE is set and does not exist..
+    if [[ -n "$PROXY_CA_FILE" ]] && [ ! -f "$PROXY_CA_FILE" ]; then
+        if [[ -n "$PROXY_CA_URL" ]]; then
+            # Get CA via specified URL
+            until curl --output /dev/null --silent --head --fail "$PROXY_HOST:$PROXY_PORT"; do
+                printf 'Waiting for proxy...'
+                sleep 1
+            done
+
+            curl -x "$PROXY_HOST:$PROXY_PORT"  "$PROXY_CA_URL" > "$PROXY_CA_FILE"
+        fi
     fi
 
     export http_proxy=http://$PROXY_HOST:$PROXY_PORT
